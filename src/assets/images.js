bef3365128fc19f7e566b750a3ab658e8f44cd05
Dokyun Lim <im.koremp@gmail.com>
2020-12-26 02:16:56 +0900

    Add assets

---
+++ src/assets/images.js
@@ -0,0 +1,33 @@
+import GidleLogoImage from './images/logo/logo_gidle.png';
+
+import MiyeonImage from './images/members/MiyeonImage.png'
+import MinnieImage from './images/members/MinnieImage.png'
+import SoojinImage from './images/members/SoojinImage.png'
+import SoyeonImage from './images/members/SoyeonImage.png'
+import YuqiImage from './images/members/YuqiImage.png'
+import ShuhuaImage from './images/members/ShuhuaImage.png'
+
+import InstagramImage from './images/social/Instagram.png'
+import TwitterImage from './images/social/Twitter.png'
+import YoutubeImage from './images/social/Youtube.png'
+import VLIVEImage from './images/social/VLIVE.png'
+
+export {
+  GidleLogoImage,
+};
+
+export const membersImages = {
+  1: MiyeonImage,
+  2: MinnieImage,
+  3: SoojinImage,
+  4: SoyeonImage,
+  5: YuqiImage,
+  6: ShuhuaImage,
+};
+
+export const socialImages = {
+  1: InstagramImage,
+  2: TwitterImage,
+  3: YoutubeImage,
+  4: VLIVEImage,
+}
