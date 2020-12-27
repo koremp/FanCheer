01557f985788d56170fd6ddf229aa1a88af3f762
Dokyun Lim <im.koremp@gmail.com>
2020-12-28 01:45:01 +0900

    MemberDetail Component 추가

---
+++ src/component/MemberDetail.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import styled from '@emotion/styled'
+
+import { membersImages } from '../assets/images';
+
+const Container = styled.div({
+
+});
+
+const Name = styled.div({
+  alignContent: 'center'
+})
+
+const ProfileImage = styled.img({
+  display: 'block',
+  height: '15em',
+  margin: '1em',
+  border: 0,
+  boxShadow: '0 5px 5px -5px rgba(0,0,0,.5)'
+})
+
+const Role = styled.div({
+
+});
+
+const Description = styled.div({
+
+});
+
+export default function MemberDetail({ member }) {
+  const {
+    id,
+    korName,
+    engName,
+    backgroundColor,
+    color,
+    roles,
+    birthday,
+    description
+  } = member;
+
+  return (
+    <Container>
+      <ProfileImage
+        src={membersImages[id]}
+        alt=""
+      />
+      <Name>{korName}({engName})</Name>
+      {
+        roles.map((role) => (
+          <Role>
+            {role}
+          </Role>
+        ))
+      }
+    </Container>
+  )
+}
