17ac9c93f2f87a4f58514ec722c969827dc62520
Dokyun Lim <im.koremp@gmail.com>
2020-12-29 23:27:56 +0900

    webpack config 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle-[hash].js',
+    publicPath: '/',
+  },
   module: {
     rules: [
       {
@@ -19,8 +25,11 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    historyApiFallback: {
-      index: 'index.html',
-    },
+    historyApiFallback: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
 };
