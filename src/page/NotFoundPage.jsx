0f55343f079d50dfe8d23e52c9e4afd190606832
Dokyun Lim <im.koremp@gmail.com>
2020-12-30 23:28:01 +0900

    Update NotFoundPage

--- src/page/NotFoundPage.jsx
+++ src/page/NotFoundPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+
+import BackToHomeButton from '../component/members/BackToHomeButton';
+
 export default function NotFoundPage() {
+  const history = useHistory();
+
+  function handleClick(url) {
+    history.push(url);
+  }
+
   return (
     <div>
-      NotFoundPage
+      asdf
+      <BackToHomeButton
+        onClick={handleClick}
+      />
     </div>
   );
 }
