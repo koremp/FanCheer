67914a0d804e9a6cdf861c2437a4c11ea2cb938a
Dokyun Lim <im.koremp@gmail.com>
2020-12-30 23:22:46 +0900

    Update index.jsx
    BrowserRouter의 basename을 BASE_PATH로 수정

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.BASE_PATH}>
         <App />
       </BrowserRouter>
     </Provider>
