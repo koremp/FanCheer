21405259b9dca4f89badbd0ac998fe9d5ab5bfe3
Dokyun Lim <im.koremp@gmail.com>
2020-12-31 03:04:40 +0900

    Set base and base path

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter basename="/fancheer/">
+      <BrowserRouter basename={process.env.BASE_PATH}>
         <App />
       </BrowserRouter>
     </Provider>
