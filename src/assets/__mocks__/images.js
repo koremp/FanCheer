7e69c0064e666ed7be1b8ba34794e2e4b349e92a
Dokyun Lim <im.koremp@gmail.com>
2021-01-07 05:23:51 +0900

    Update mock assets

---
+++ src/assets/__mocks__/images.js
@@ -0,0 +1,79 @@
+const GidleLogoImage = '';
+const GidleGroupImage = '';
+const NamuwikiLogoImage = '';
+const WikipediaLogoImage = '';
+
+export {
+  GidleLogoImage,
+};
+
+export {
+  GidleGroupImage,
+};
+
+export {
+  NamuwikiLogoImage,
+  WikipediaLogoImage,
+};
+
+export const membersImages = {
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+};
+
+export const socialImages = {
+  instagram: '',
+  twitter: '',
+  youtube: '',
+  vlive: '',
+  tiktok: '',
+  homepage: '',
+  cafe: '',
+};
+
+export const cheerSongImages = {
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+  7: '',
+};
+
+export const korMiniAlbumImages = {
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+};
+
+export const korSingleImages = {
+  1: '',
+};
+
+export const korDigitalSingleImages = {
+  1: '',
+  2: '',
+  3: '',
+};
+
+export const etcAlbumImages = {
+  QueenDom: {
+    1: '',
+    2: '',
+  },
+  KDA: {
+    1: '',
+    2: '',
+    3: '',
+  },
+};
+
+export const entImages = {
+  cube: '',
+};
