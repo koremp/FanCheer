f7996a6d9443eef07ed282b1f7c258911e87120f
Dokyun Lim <im.koremp@gmail.com>
2021-01-07 04:55:03 +0900

    Update assets

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,10 @@
 import GidleLogoImage from './images/logo/logo_gidle.png';
 
+import GidleGroupImage from './images/group/gidle.png';
+
+import NamuwikiLogoImage from './images/wiki/Namu.png';
+import WikipediaLogoImage from './images/wiki/Wikipedia.png';
+
 import MiyeonImage from './images/members/MiyeonImage.png';
 import MinnieImage from './images/members/MinnieImage.png';
 import SoojinImage from './images/members/SoojinImage.png';
@@ -7,13 +12,43 @@ import SoyeonImage from './images/members/SoyeonImage.png';
 import YuqiImage from './images/members/YuqiImage.png';
 import ShuhuaImage from './images/members/ShuhuaImage.png';
 
-import InstagramImage from './images/social/Instagram.png';
-import TwitterImage from './images/social/Twitter.png';
+import HomepageImage from './images/social/Site.png';
 import YoutubeImage from './images/social/Youtube.png';
+import TwitterImage from './images/social/Twitter.png';
+import InstagramImage from './images/social/Instagram.png';
+import TikTokImage from './images/social/TikTok.png';
 import VLIVEImage from './images/social/VLIVE.png';
+import CafeImage from './images/social/Cafe.png';
+
+import DumdidumdiImage from './images/albums/DUMDiDUMDi.png';
+import HannImage from './images/albums/HANN.png';
+import IamImage from './images/albums/IAM.png';
+import IburnImage from './images/albums/IBURN.png';
+import ImadeImage from './images/albums/IMADE.png';
+import ImthetrendImage from './images/albums/IMTHETREND.png';
+import ItrustImage from './images/albums/ITRUST.png';
+import UhohImage from './images/albums/Uhoh.png';
+
+import QueendomPartOneImage from './images/albums/QueendomPt1.png';
+import QueendomFinalImage from './images/albums/QueendomFinal.png';
+
+import KDAPopstarsImage from './images/albums/KDA_POPSTARS.png';
+import KDATheBaddestImage from './images/albums/KDA_THEBADDEST.png';
+import KDAMoreImage from './images/albums/KDA_MORE.png';
+
+import CubeEntImage from './images/ent/cube.png';
 
 export {
   GidleLogoImage,
+};
+
+export {
+  GidleGroupImage,
+};
+
+export {
+  NamuwikiLogoImage,
+  WikipediaLogoImage,
 };
 
 export const membersImages = {
@@ -26,8 +61,54 @@ export const membersImages = {
 };
 
 export const socialImages = {
-  1: InstagramImage,
-  2: TwitterImage,
-  3: YoutubeImage,
-  4: VLIVEImage,
+  instagram: InstagramImage,
+  twitter: TwitterImage,
+  youtube: YoutubeImage,
+  vlive: VLIVEImage,
+  tiktok: TikTokImage,
+  homepage: HomepageImage,
+  cafe: CafeImage,
+};
+
+export const cheerSongImages = {
+  1: IamImage,
+  2: HannImage,
+  3: ImadeImage,
+  4: UhohImage,
+  5: QueendomFinalImage,
+  6: ItrustImage,
+  7: DumdidumdiImage,
+};
+
+export const korMiniAlbumImages = {
+  1: IamImage,
+  2: ImadeImage,
+  3: ItrustImage,
+  4: IburnImage,
+};
+
+export const korSingleImages = {
+  1: DumdidumdiImage,
+};
+
+export const korDigitalSingleImages = {
+  1: HannImage,
+  2: UhohImage,
+  3: ImthetrendImage,
+};
+
+export const etcAlbumImages = {
+  QueenDom: {
+    1: QueendomPartOneImage,
+    2: QueendomFinalImage,
+  },
+  KDA: {
+    1: KDAMoreImage,
+    2: KDATheBaddestImage,
+    3: KDAMoreImage,
+  },
+};
+
+export const entImages = {
+  cube: CubeEntImage,
 };
