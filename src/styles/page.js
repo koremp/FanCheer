6cfc7c8e992bb7a302f7123e0558a57da3f7b150
Dokyun Lim <im.koremp@gmail.com>
2021-01-07 05:39:44 +0900

    page style 추가

---
+++ src/styles/page.js
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+
+const HomeLayout = styled.div({
+  width: '100%',
+  height: '100%',
+  backgroundImage: `linear-gradient(
+    60deg,
+    #E11900,
+    #7E00BF
+  )`,
+  backgroundAttachment: 'fixed',
+});
+
+const FullContainer = styled.div({
+  width: '100%',
+  height: '100%',
+  backgroundImage: `linear-gradient(
+    60deg,
+    #E11900,
+    #7E00BF
+  )`,
+  backgroundAttachment: 'fixed',
+});
+
+const OverContainer = styled.div({
+  width: '100%',
+  margin: '0',
+  backgroundImage: `linear-gradient(
+    60deg,
+    #E11900,
+    #7E00BF
+  )`,
+  backgroundAttachment: 'fixed',
+});
+
+export {
+  HomeLayout,
+  FullContainer,
+  OverContainer,
+};
