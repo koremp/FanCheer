7e8ba18324198edda5de50a0e5acfc8a031748fe
Dokyun Lim <im.koremp@gmail.com>
2021-01-07 05:43:12 +0900

    CountDown 추가

---
+++ src/container/CountDownContainer.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+import CountDown from '../component/CountDown';
+
+export default function CountDownContainer({ handleClickLink }) {
+  return (
+    <CountDown />
+  );
+}
