c07b239e70008194a1b0b8a37194682b281b63dc
Dokyun Lim <im.koremp@gmail.com>
2021-01-07 05:24:44 +0900

    AboutPage 테스트 추가

--- src/page/AboutPage.test.jsx
+++ src/page/AboutPage.test.jsx
@@ -6,12 +6,12 @@ import { render } from '@testing-library/react';
 
 import AboutPage from './AboutPage';
 
+import about from '../../fixtures/about';
+
 jest.mock('../assets/images');
 
 test('AboutPage', () => {
   const { getByText } = render((
-    <MemoryRouter>
-      <AboutPage />
-    </MemoryRouter>
+    <AboutPage />
   ));
 });
