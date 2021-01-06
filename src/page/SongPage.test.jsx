76c1aea673336f32c22134191f80008506eafe7a
Dokyun Lim <im.koremp@gmail.com>
2021-01-07 05:37:58 +0900

    SongPage test 추가

--- src/page/SongPage.test.jsx
+++ src/page/SongPage.test.jsx
@@ -6,14 +6,14 @@ import { render } from '@testing-library/react';
 
 import SongPage from './SongPage';
 
+import songList from '../../fixtures/songList';
+
 jest.mock('../assets/images');
 
 test('SongPage', () => {
   const { getByText } = render((
     <MemoryRouter>
-      <SongPage />
+      <SongPage match={{ params: { id: 1 } }} />
     </MemoryRouter>
   ));
-
-  expect(getByText(/SongPage/)).not.toBeNull();
 });
