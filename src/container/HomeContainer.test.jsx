380fee874a0d7d68dc2ad5133d2a59222ae8eafc
Dokyun Lim <im.koremp@gmail.com>
2021-01-08 19:32:04 +0900

    HomeContainer test 추가

---
+++ src/container/HomeContainer.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import HomeContainer from './HomeContainer';
+
+jest.mock('../assets/images');
+
+test('HomeContainer', () => {
+  const handleClickLink = jest.fn();
+
+  const { getByAltText } = render((
+    <HomeContainer
+      handleClickLink={handleClickLink}
+    />
+  ));
+});
