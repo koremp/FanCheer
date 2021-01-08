3a596b1fa2ead7d4e7638cadda5180600eb32e12
Dokyun Lim <im.koremp@gmail.com>
2021-01-08 19:31:10 +0900

    AboutContainer test 추가

---
+++ src/container/AboutContainer.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import AboutContainer from './AboutContainer';
+
+import about from '../../fixtures/about';
+import members from '../../fixtures/members';
+
+jest.mock('../assets/images');
+
+test('AboutContainer', () => {
+  const handleClick = jest.fn();
+
+  const { getByAltText } = render((
+    <AboutContainer
+      about={about}
+      members={members}
+      handleClick={handleClick}
+    />
+  ));
+});
