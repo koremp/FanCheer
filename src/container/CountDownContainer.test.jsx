2575dc0473eccb9c8c504894190f64ae3ddce1d6
Dokyun Lim <im.koremp@gmail.com>
2021-01-08 19:31:53 +0900

    CountDownContainer test 추가

---
+++ src/container/CountDownContainer.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import CountDownContainer from './CountDownContainer';
+
+jest.mock('../assets/images');
+
+test('CountDownContainer', () => {
+  const handleClick = jest.fn();
+
+  const { getByAltText } = render((
+    <CountDownContainer
+      handleClick={handleClick}
+    />
+  ));
+});
