ab24df4bba994d9c94795af5ac5eb7d6eb4c1155
Dokyun Lim <im.koremp@gmail.com>
2021-01-08 19:32:13 +0900

    MemberDetailContainer test 추가

---
+++ src/container/MemberDetailContainer.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import MemberDetailContainer from './MemberDetailContainer';
+
+import members from '../../fixtures/members';
+
+jest.mock('../assets/images');
+
+test('MemberDetailContainer', () => {
+  const member = members.find((_) => _.id === 1);
+
+  const { getByAltText } = render((
+    <MemberDetailContainer member={member} />
+  ));
+});
