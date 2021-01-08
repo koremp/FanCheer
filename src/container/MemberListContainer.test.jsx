d07ecb751b0b4d486d70bcae7d48b691182ad27c
Dokyun Lim <im.koremp@gmail.com>
2021-01-08 19:32:22 +0900

    MemberListContainer test 추가

---
+++ src/container/MemberListContainer.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import MemberListContainer from './MemberListContainer';
+
+import members from '../../fixtures/members';
+
+jest.mock('../assets/images');
+
+test('MemberListContainer', () => {
+  const handleClick = jest.fn();
+
+  const { getByAltText } = render((
+    <MemberListContainer
+      members={members}
+      handleClick={handleClick}
+    />
+  ));
+});
