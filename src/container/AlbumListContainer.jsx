163858b4b560da280bde1b710e99c1f8918ab956
Dokyun Lim <im.koremp@gmail.com>
2021-01-11 03:26:06 +0900

    Update container

--- src/container/AlbumListContainer.jsx
+++ src/container/AlbumListContainer.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
 
-import styled from '@emotion/styled';
-
 import AlbumList from '../component/AlbumList';
-
-import {
-  korMiniAlbumImages,
-  korSingleImages,
-  korDigitalSingleImages,
-  etcAlbumImages,
-} from '../assets/images';
-
-const Container = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  borderRadius: '1em',
-});
 
 export default function AlbumListContainer({
   korMiniAlbums,
@@ -25,45 +9,13 @@ export default function AlbumListContainer({
   etcAlbums,
   handleClick,
 }) {
-  const korMiniAlbumIds = [1, 2, 3, 4];
-  const korSingleIds = [1];
-  const korDigitalSingleIds = [1, 2, 3];
-  const etcAlbumIds = [1, 2, 3, 4];
-
   return (
-    <Container>
-      <AlbumList
-        name="한국 미니 앨범"
-        ids={korMiniAlbumIds}
-        albums={korMiniAlbums}
-        images={korMiniAlbumImages}
-        handleClick={handleClick}
-      />
-      <AlbumList
-        name="한국 싱글"
-        ids={korSingleIds}
-        albums={korSingles}
-        images={korSingleImages}
-        handleClick={handleClick}
-      />
-      <AlbumList
-        name="한국 디지털 싱글"
-        ids={korDigitalSingleIds}
-        albums={korDigitalSingles}
-        images={korDigitalSingleImages}
-        handleClick={handleClick}
-      />
-      {
-        false && (
-          <AlbumList
-            name="기타"
-            ids={etcAlbumIds}
-            albums={etcAlbums}
-            images={etcAlbumImages}
-            handleClick={handleClick}
-          />
-        )
-      }
-    </Container>
+    <AlbumList
+      korMiniAlbums={korMiniAlbums}
+      korSingles={korSingles}
+      korDigitalSingles={korDigitalSingles}
+      etcAlbums={etcAlbums}
+      handleClick={handleClick}
+    />
   );
 }
--- src/container/CheerSongListContainer.jsx
+++ src/container/CheerSongListContainer.jsx
@@ -5,11 +5,8 @@ import CheerSongList from '../component/CheerSongList';
 import songList from '../../fixtures/songList';
 
 export default function CheerSongContainer({ handleClick }) {
-  const ids = [1, 2, 3, 4, 5, 6, 7];
-
   return (
     <CheerSongList
-      ids={ids}
       songList={songList}
       handleClick={handleClick}
     />
