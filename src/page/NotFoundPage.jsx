3a82254d808b0a2e5bf3defd36d699832cb344ed
Dokyun Lim <im.koremp@gmail.com>
2024-08-19 02:38:50 +0900

    feat: add style to not found page

--- src/page/NotFoundPage.jsx
+++ src/page/NotFoundPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 import { useNavigate } from 'react-router-dom';
 
 import BackToHomeButton from '../component/BackToHomeButton';
+import { FullContainer } from '../styles/page';
+
+const NotFoundContainer = styled.div({
+  margin: 'auto',
+})
+
+const NotFoundText = styled.h1({
+  fontSize: '2em'
+})
 
 export default function NotFoundPage() {
   const nav = useNavigate();
@@ -12,11 +23,15 @@ export default function NotFoundPage() {
   }
 
   return (
-    <div>
-      Not Found
+    <FullContainer>
+      <NotFoundContainer>
+        <NotFoundText>
+          404 Not Found
+        </NotFoundText>
+      </NotFoundContainer>
       <BackToHomeButton
         onClick={handleClick}
       />
-    </div>
+    </FullContainer>
   );
 }
