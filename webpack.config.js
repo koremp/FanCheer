c8df0eeba3e469e971458aa285e6b21afa661317
Dokyun Lim <im.koremp@gmail.com>
2024-08-31 01:11:06 +0900

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const development = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle-[hash].js',
-    // publicPath: 'https://koremp.github.io/FanCheer/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: 'file-loader',
-      },
+const setConfig = ({ isProduction }) => {
+  const config = {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle-[hash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: 'file-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        filename: 'index.html',
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html'),
-    }),
-  ],
-};
+    devServer: {
+      historyApiFallback: true,
+    },
+  };
 
-const production = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle-[hash].js',
-    publicPath: '/fancheer/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: 'file-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './dist/index.html'),
-    }),
-  ],
+  return isProduction ? { ...config, output: { publicPath: '/fancheer/' } } : { ...config };
+
 };
 
 module.exports = (env, argv) => {
   const { mode } = argv;
 
-  if (mode === 'development') {
-    return development;
-  }
+  const isProduction = mode === 'production';
 
-  if (mode === 'production') {
-    return production;
-  }
-
-  if (mode === 'none') {
-
-  }
+  return setConfig({ isProduction })
 };
