66e2e84ba5a45d08c6da3875922a527ddb911fc7
Dokyun Lim <im.koremp@gmail.com>
2024-09-02 16:53:27 +0900

    test: cover util tests in describe func

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,34 +1,36 @@
 import { get, equal, zeroPad } from '../utils';
 
-test('get', () => {
-  const state = {
-    name: '홍길동',
-  };
+describe('utils', () => {
+  test('get', () => {
+    const state = {
+      name: '홍길동',
+    };
 
-  const f = get('name');
-  const g = get('age');
+    const f = get('name');
+    const g = get('age');
 
-  expect(f(state)).toBe('홍길동');
-  expect(g(state)).toBeUndefined();
-});
+    expect(f(state)).toBe('홍길동');
+    expect(g(state)).toBeUndefined();
+  });
 
-test('equal', () => {
-  const state = {
-    name: '홍길동',
-  };
+  test('equal', () => {
+    const state = {
+      name: '홍길동',
+    };
 
-  const f = equal('name', '홍길동');
-  const g = equal('name', '임꺽정');
+    const f = equal('name', '홍길동');
+    const g = equal('name', '임꺽정');
 
-  expect(f(state)).toBeTruthy();
-  expect(g(state)).toBeFalsy();
-});
+    expect(f(state)).toBeTruthy();
+    expect(g(state)).toBeFalsy();
+  });
 
-test('zeroPad', () => {
-  const number = 3;
-  const base = 10;
+  test('zeroPad', () => {
+    const number = 3;
+    const base = 10;
 
-  const result = zeroPad(number, base);
+    const result = zeroPad(number, base);
 
-  expect(result).toEqual('03');
+    expect(result).toEqual('03');
+  });
 });
